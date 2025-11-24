93a8e44da16bb78a577e6595fdb47ff44de04041
MoX678 <mk6547719@gmail.com>
2025-11-24 11:29:08 +0200

    Inject dynamic baseUrl into Docusaurus config for pull requests

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -18,11 +18,10 @@ const config = {
   url: 'https://github.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/doc/',
+  baseUrl: '/docs2/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'metacall', // Usually your GitHub org/user name.
   projectName: 'doc', // Usually your repo name.
   deploymentBranch: 'gh-pages',
 
